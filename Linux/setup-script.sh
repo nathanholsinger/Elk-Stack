dda500b5b46f3d2f362fa82be1cccfe872f35808
Nathan Holsinger <51317767+nathanholsinger@users.noreply.github.com>
2021-11-18 18:16:56 -0700

    Create setup-script.sh

---
+++ Linux/setup-script.sh
@@ -0,0 +1,9 @@
+cat >> /home/sysadmin/.bashrc << END
+Custom Aliases
+alias reload='source ~/.bashrc && echo Bash config reloaded'
+alias lsa='ls -a'
+alias docs='cd ~/Documents'
+alias dwn='cd ~/Downloads'
+alias etc='cd /etc'
+alias rc='nano ~/.bashrc'
+END
